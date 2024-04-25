40107160f7b1efc69d658046c2da4a1af38a053d
MissingMirkea <160932114+MissingMirkea@users.noreply.github.com>
2024-04-25 11:03:31 +0300

    Added comments

--- SteamLabel/Form1.cs
+++ SteamLabel/Form1.cs
@@ -16,12 +16,13 @@ namespace SteamLabel
     public partial class Form1 : Form
     {
         SqlConnection con = new SqlConnection("Data Source=DESKTOP-UBC4JA8\\SQLEXPRESS;Initial Catalog=SteamLabel;Integrated Security=True;");
-
+        //Conectarea catre BD
         public Form1()
         {
             InitializeComponent();
             this.StartPosition = FormStartPosition.CenterScreen;
             panel2.Hide();
+            //Pozitia de start sa fie in centrul ecranului , 
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -41,13 +42,16 @@ namespace SteamLabel
 
         private void button2_Click(object sender, EventArgs e)
         {
+            //Preluarea datelor din text box-uri
             string login = textBox1.Text;
             string password = textBox3.Text;
             string mail = textBox2.Text;
             string email = @"^[a-zA-Z0-9._%+-]+@(?:\bmail\b|\bgmail\b|\bhotmail\b|\byahoo\b|\boutlook\b|\b[a-zA-Z0-9.-]+\.com\b)$";
+            //Controlam daca in textbox 2 mail-ul e dupa un format acceptat 
             Regex regex = new Regex(email);
             if (regex.IsMatch(textBox2.Text))
             {
+                
                 con.Open();
                 if (textBox3.Text == textBox4.Text && textBox3.Text != null)
                 {
@@ -55,7 +59,7 @@ namespace SteamLabel
                     string title = "Close Window";
                     MessageBoxButtons buttons = MessageBoxButtons.OK;
                     MessageBox.Show(message, title, buttons);
-
+                    //daca totul e corect introdus , afisam ca accountul a fost creat
                 }
                 else
                 {
@@ -63,7 +67,9 @@ namespace SteamLabel
                     string title = "Close Window";
                     MessageBoxButtons buttons = MessageBoxButtons.RetryCancel;
                     MessageBox.Show(message, title, buttons);
+                        //daca e introdus gresit afisam ca este o eroare la password
                 }
+                //daca totul e good , atunci inseram
                 SqlCommand insertcmd = new SqlCommand("INSERT INTO Registration (Login,Mail,Password) values (@Login , @Mail , @Password)", con);
                 insertcmd.Parameters.AddWithValue("@Login" , login);
                 insertcmd.Parameters.AddWithValue("Mail" ,mail);
@@ -73,6 +79,8 @@ namespace SteamLabel
             }
             else
             {
+                //daca e introdus gresit afisam ca este o eroare la mail,format gresit etc.
+
                 string message = "Invalid mail type";
                 string title = "Close Window";
                 MessageBoxButtons buttons = MessageBoxButtons.OK;
@@ -84,29 +92,35 @@ namespace SteamLabel
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //schimbam panelul spre acel de creare a contului
             panel2.Show();
            panel2.BringToFront();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            //logarea 
             string login = textBox8.Text;
             string password = textBox6.Text;
             try
             {
+                con.Open();
+                //folosim select pentru a controla daca acest accountul este registrat in BD
                 String query = "SELECT *FROM Registration WHERE login ='" + textBox8.Text + "' AND PASSWORD = '" + textBox6.Text + "'";
                 SqlDataAdapter sda = new SqlDataAdapter(query, con);
                 DataTable dataT = new DataTable();
                 sda.Fill(dataT);
                 if (dataT.Rows.Count > 0)
                 {
+                    //daca este , atunci conectarea e cu succes
                     string message = "Success";
                     string title = "Close Window";
                     MessageBoxButtons buttons = MessageBoxButtons.OK;
                     MessageBox.Show(message, title, buttons);
                 }
                 else
-                {
+                { 
+                    //else eror
                     string message = "Login or Password wrong";
                     string title = "Close Window";
                     MessageBoxButtons buttons = MessageBoxButtons.RetryCancel;
@@ -116,6 +130,7 @@ namespace SteamLabel
             }
             finally
             {
+                //In orice caz inchidem conectiunea 
                 con.Close();
             }
         }
